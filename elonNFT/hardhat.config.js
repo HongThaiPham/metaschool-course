88360b7d863b5c95000ad78b77f98b008faa65b5
Pham Hong Thai <hongthaipro@gmail.com>
2022-04-25 22:15:27 +0700

    elonNFT

--- elonNFT/hardhat.config.js
+++ elonNFT/hardhat.config.js
@@ -1,6 +1,21 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("dotenv").config(); //all the key value pairs are being made available due to this lib
+require("@nomiclabs/hardhat-ethers");
+const { API_URL_KEY, PRIVATE_KEY, POLYGON_API_URL_KEY } = process.env; //environment variables are being loaded here.
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.1",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    matic: {
+      url: POLYGON_API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
